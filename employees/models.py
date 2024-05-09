822a67abd7b5c379c7d71be0d47dc454b11ab76f
serhatbalik <mserhatbalik@gmail.com>
2024-05-09 21:07:47 +0300

    Created employee model and registerd it in Admin panel for use

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from django.db import models  # Importing models module from django.db
 
-# Create your models here.
+# Defining Employee model which inherits from models.Model
+class Employee(models.Model):
+    name = models.CharField(max_length=100)  # Name field, a character field with maximum length of 100
+    last_name = models.CharField(max_length=100)  # Last name field, a character field with maximum length of 100
+    photo = models.ImageField(upload_to='images')  # Photo field, an image field where images will be uploaded to 'images' directory
+    designation = models.CharField(max_length=100)  # Designation field, a character field with maximum length of 100
+    email = models.EmailField(max_length=100, unique=True)  # Email field, an email field with maximum length of 100, must be unique
+    phone_number = models.CharField(max_length=12, blank=True)  # Phone number field, a character field with maximum length of 12, can be blank
+    created_at = models.DateTimeField(auto_now_add=True)  # Created at field, a datetime field which is automatically set to the current date and time when the object is first created
+    updated_at = models.DateTimeField(auto_now=True)  # Updated at field, a datetime field which is automatically set to the current date and time whenever the object is saved
