851cf172e7e5b0d6222e67ce7685c8e38994a09e
serhatbalik <mserhatbalik@gmail.com>
2024-05-09 19:47:22 +0300

    Created static files and a new app, also implemented Bootstrap

--- djangoBeginnerProject/settings.py
+++ djangoBeginnerProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "employees.apps.EmployeesConfig"
 ]
 
 MIDDLEWARE = [
@@ -117,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR /'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'djangoBeginnerProject/static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
